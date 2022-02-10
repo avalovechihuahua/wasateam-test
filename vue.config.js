bab492901b388409300cdeddc5ea1f80198c1c5b
Ava <avatzeng@gmail.com>
2022-02-10 16:21:41 +0800

    [ 功能 ] 加入 prerender 功能

---
+++ vue.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const PrerenderSpaPlugin = require("prerender-spa-plugin");
+const isProduction = process.env.NODE_ENV === "production";
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  outputDir: "dist",
+  assetsDir: "static",
+  productionSourceMap: false,
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+          @use "@/assets/style/abstract/_index.scss" as *;
+        `,
+      },
+    },
+  },
+  configureWebpack: (config) => {
+    if (isProduction) {
+      config.plugins.push(
+        // 預渲染外掛配置
+        new PrerenderSpaPlugin({
+          // 靜態資源路徑
+          staticDir: path.join(__dirname, "dist"),
+          // 預渲染路由
+          // 如有動態param請完整定義在這裡
+          routes: ["/", "/theme"],
+        })
+      );
+    }
+  },
+};
